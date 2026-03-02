769fbf135485712b6998111b5ab82d2db690dc53
Николай <nikolaj.novikov@topvisor.com>
2026-03-02 22:52:02 +0300

    1 задание

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const PORT = 3000;
 
+
 app.get('/1', (req, res) => {
     res.set('X-Author', 'sedm1')
+    res.set('Access-Control-Allow-Origin', '*')
     res.send('sedm1');
 });
 
