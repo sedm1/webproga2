e8871c622d079b34ca2de688002bbc3e9dc54943
Николай <nikolaj.novikov@topvisor.com>
2026-03-02 23:06:35 +0300

    2 задание

--- src/index.js
+++ src/index.js
@@ -25,11 +25,14 @@ function task(x) {
 app.get('/sample/', (req, res) => {
     res.set('Content-Type', 'text/plain; charset=UTF-8');
 
-    const x = +req.query.x || 0;
-    const result = task.call(Number(x), Number(x));
-    res.send(result.toString());
-});
+    const functionCode = `
+        function task(x) {
+            return x * (this * this);
+        }
+    `;
 
+    res.send(functionCode);
+});
 
 app.listen(PORT, () => {
     console.log(`Запуск на порту ${PORT}`);
