2fd443418a124ea8f941149836f8e888adebab22
Николай <nikolaj.novikov@topvisor.com>
2026-03-02 22:59:08 +0300

    2 задание

--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ const app = express();
 
 const PORT = 3000;
 
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
 
+// 1 задание 1 лабы
 app.get('/1', (req, res) => {
     res.set('X-Author', 'sedm1')
-    res.set('Access-Control-Allow-Origin', '*')
     res.send('sedm1');
 });
+
+// 2 задание первой лабы
+app.get('/login/', (req, res) => {
+    res.set('Content-Type', 'text/plain; charset=UTF-8')
+})
+function task(x) {
+    return x * (this * this);
+}
+app.get('/sample/', (req, res) => {
+    const x = +req.query.x || 0;
+    const result = task.call(Number(x), Number(x));
+    res.send(result);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Запуск на порту ${PORT}`);
