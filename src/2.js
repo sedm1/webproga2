f9df42aec5fa2387dcb23ab7e25bd3bc95d26c8b
Николай <nikolaj.novikov@topvisor.com>
2026-03-22 14:58:13 +0300

    test

--- src/2.js
+++ src/2.js
@@ -23,4 +23,33 @@ module.exports = function registerLab2Endpoints(app) {
             'x-body': req.body
         }));
     });
+
+    app.get('/:dateCode', (req, res, next) => {
+        const { dateCode } = req.params;
+        if (!/^\d{6}$/.test(dateCode)) {
+            return next();
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify({
+            date: today(),
+            login: 'sedm1'
+        }));
+    });
+
+    app.get('/api/rv/:abc', (req, res) => {
+        const { abc } = req.params;
+
+        const reversed = abc.split('').reverse().join('');
+        return res.send(reversed);
+    });
 };
+
+function today() {
+    const now = new Date();
+    const dd = String(now.getDate()).padStart(2, '0');
+    const mm = String(now.getMonth() + 1).padStart(2, '0');
+    const yyyy = String(now.getFullYear());
+
+    return `${dd}-${mm}-${yyyy}`;
+}
