d61696454f8750de99e053104b8e62d940735b49
Николай <nikolaj.novikov@topvisor.com>
2026-05-08 17:31:09 +0300

    Марощрутизация

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {createReadStream} from 'node:fs';
-import crypto from 'node:crypto';
-import http from 'node:http';
-import {appSrc} from './app.js';
+import express from "express";
+const app = express();
+const PORT = 3000;
+const LOGIN = "sedm1";
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+function todayRoute() {
+    const now = new Date();
+    const dd = String(now.getDate()).padStart(2, "0");
+    const mm = String(now.getMonth() + 1).padStart(2, "0");
+    const yy = String(now.getFullYear()).slice(-2);
+    return `/${dd}${mm}${yy}`;
+}
 
-app.listen(3000);
+app.get(todayRoute(), (req, res) => {
+    res.type("text/plain").send(LOGIN);
+});
+
+app.get("/add/:x1/:x2", (req, res) => {
+    const x1 = Number(req.params.x1);
+    const x2 = Number(req.params.x2);
+    res.type("text/plain").send(String(x1 + x2));
+});
+
+app.get("/mpy/:y1/:y2", (req, res) => {
+    const y1 = Number(req.params.y1);
+    const y2 = Number(req.params.y2);
+    res.type("text/plain").send(String(y1 * y2));
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}/`);
+});
